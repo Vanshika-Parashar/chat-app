de71f9a93faccc940d6f1a382e6200a1286022cc
vanshika parashar <parasharvanshika2005@gmail.com>
2026-06-01 17:45:02 +0530

    more message funtionality added

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,6 +1,7 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
-
+import cloudinary from "../lib/cloudinary.js";
+import{io,userSocketMap} from "../server.js";
 
 //Get all users except logged in user
 export const getUsersForSidebar=async(req,res)=>{
@@ -61,4 +62,33 @@ export const markMessageAsSeen=async(req,res)=>{
 
     }
 }
+//send message to selected user
+export const sendMessage=async(req,res)=>{
+    try{
+        const{text,image}=req.body;
+        const receiverId=req.params.id;
+        const senderId=req.user._id;
 
+        let imageUrl;
+        if(image){
+            const uploadResponse=await cloudinary.uploader.upload(image)
+            imageUrl=uploadResponse.secure_url;
+        }
+        const newMessage=await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image:imageUrl
+        })
+         //emit new message to the receiver's socket
+         const receiverSocketId=userSocketMap[receiverId];
+         if(receiverSocketId){
+            io.to(receiverSocketId).emit("newMessage",newMessage)
+         }
+        res.json({success:true,message:newMessage});
+    }catch(error){
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+
+    }
+}
--- server/server.js
+++ server/server.js
@@ -5,10 +5,38 @@ import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
+import {Server} from "socket.io";
 
 //create express app and HTTP server
 const app=express();
 const server =http.createServer(app);
+
+//Initialize socket.io server
+export const io=new Server(server,{
+    cors:{origin:"*"}
+})
+
+//store online users
+export const userSocketMap={};//{userId:socketId}
+
+//socket.io connection handler
+io.on("connection",()=>{
+    const userId=socket.handshake.query.userId;
+    console.log("User Connected", userId);
+
+   if(userId) userSocketMap[userId]=socket.id;
+
+   //Emit online users to all conected clients
+   io.emit("getOnlineUsers",Object.keys(userSocketMap));
+
+   socket.on("disconnect",()=>{
+        console.log("User Disconnected", userId);
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers",Object.keys(userSocketMap))
+   })
+
+})
+
 //middleware setup
 app.use(express.json({limit:"4mb"}));
 app.use(cors());
